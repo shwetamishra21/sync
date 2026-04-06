132e3026eb6f9312a8af8dcc0a59514971f4fb1a
Shweta Mishra <mishrashweta0220@gmail.com>
2026-04-07 04:27:19 +0530

    upload project

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +23,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(color = Color.Cyan) {
+                        Greeting(
+                            name = "Aditi",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
@@ -33,8 +38,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hi, my name is $name!",
+        modifier = modifier.padding(24.dp)
     )
 }
 
@@ -42,6 +47,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("Android")
+        Surface(color = Color.Cyan) {
+            Greeting("Aditi")
+        }
     }
 }
